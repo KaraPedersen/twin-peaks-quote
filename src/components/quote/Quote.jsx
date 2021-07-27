9e9356ba000819d9167992308a7d63a9bb284b1d
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-07-27 09:58:19 -0700

    created a Quote componenet and displayed name and text

--- src/components/quote/Quote.jsx
+++ src/components/quote/Quote.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const Quote = () => (
-
+const Quote = ({ name, text }) => (
+  <section>
+    <h2>{name || 'unknown'}</h2>
+    <p>{text}</p>
+  </section>
 );
-
-Quote.propTypes = {
-
-};
 
 export default Quote;
